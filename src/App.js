67af547a214ed81727cec891c36d83cbab4bc711
Anurag Rajput <anuragsinghr054@gmail.com>
2024-02-28 13:34:06 +0530

    Netflix GPT

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className='text-2xl  font-bold'>Lets Build Our Netflix - GPT </h1>
+    </div>
+  );
+}
+
+export default App;
